c8d4db6c3816e0e82c55e42a9ed8c2f10993ec59
Ben Forshey <hello@integrisweb.com>
2018-03-28 11:41:32 -0400

    typo

--- components/Pronunciation/index.js
+++ components/Pronunciation/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import propTypes from 'prop-types';
 import isEmpty from 'lodash.isempty';
 
-// This crazy API has returns different data structures, so check what I'm getting.
+// This crazy API returns different data structures, so check what I'm getting.
 const renderPronunciation = (data) => {
-  const all = Object.entries(data);
-
   if (typeof data === 'string') {
     return <p>all: {data}</p>;
   }
+
+  const all = Object.entries(data);
 
   return all.map(el => <p key={el}>{el[0]}: {el[1]}</p>);
 };
 
 const Pronunciation = ({ data }) => {
-  // Sometimes the API retuns empty objects!
+  // Sometimes the API returns empty objects!
   if (data && !isEmpty(data)) {
     return (
       <div>
