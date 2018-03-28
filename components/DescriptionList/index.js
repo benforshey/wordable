c4848841036a3fd66524812414c27c9b55dc2f70
Ben Forshey <hello@integrisweb.com>
2018-03-28 15:41:57 -0400

    move pronounce function to word, since web speech synthesis is limited

--- components/DescriptionList/index.js
+++ components/DescriptionList/index.js
@@ -3,11 +3,29 @@ import propTypes from 'prop-types';
 import Syllables from '../Syllables';
 import Pronunciation from '../Pronunciation';
 
+// TODO: visually, it is implied that pronounce refers to the word.
+// Explore what aria roles would best suit this sitation. described-by?
+// NOTE: It's a real shame that the web speech synthesis API doesn't honor
+// the International Phoenetic Alphabet, because I'd love to put this pronuncation
+// function under the pronunciation section, where it logically (and more helpfully)
+// belongs, allowing variance in emphasis.
+function speakPronunciation(e) {
+  e.preventDefault();
+  const message = new SpeechSynthesisUtterance();
+  const word = document.querySelector('[data-pronounce]').innerText;
+
+  message.text = word;
+  speechSynthesis.speak(message);
+}
+
 // NOTE: depending heavingly on conditional rendering to prevent propType ".isRequired" violations.
 const DescriptionList = ({ data }) => (
   <dl>
     <dt>
-      <h2>{data.word}</h2>
+      <h2 data-pronounce>{data.word}</h2>
+      {'speechSynthesis' in window &&
+      <button onClick={speakPronunciation}>pronounce</button>
+      }
       {data.syllables &&
       <Syllables data={data.syllables} />
       }
@@ -20,6 +38,12 @@ const DescriptionList = ({ data }) => (
       <dd key={result.definition}>{result.definition}</dd>
     ))
     }
+    <style jsx>{`
+     h2 {
+       font-size: 2.16em;
+     }
+    `}
+    </style>
   </dl>
 );
 
--- components/Syllables/index.js
+++ components/Syllables/index.js
@@ -5,20 +5,6 @@ const renderSyllables = list => list.map((syl, idx, syls) => (
   <em key={syl}>{syl}{idx + 1 < syls.length ? '-' : ''}</em>
 ));
 
-// TODO: visually, it is implied that pronounce refers to the word.
-// Explore what aria roles would best suit this sitation. described-by?
-function speakPronunciation(e) {
-  e.preventDefault();
-  const message = new SpeechSynthesisUtterance();
-  const syllables = document.querySelector('.syllables');
-  const text = syllables.innerText
-    .substring(1, syllables.innerText.length - 1)
-    .replace(/-/g, '');
-
-  message.text = text;
-  speechSynthesis.speak(message);
-}
-
 const Syllables = ({ data }) => {
   if (data) {
     return (
@@ -27,9 +13,6 @@ const Syllables = ({ data }) => {
         <p>
         ({data.count} syllables) <span className="syllables">[{renderSyllables(data.list)}]</span>
         </p>
-        {'speechSynthesis' in window &&
-        <button onClick={speakPronunciation}>pronounce</button>
-        }
       </div>
     );
   }
