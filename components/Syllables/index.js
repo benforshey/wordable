9dd08255a7c3cc7770ff46ce8dd3a1405308e686
Ben Forshey <hello@integrisweb.com>
2018-03-28 11:40:34 -0400

    add speechSyntethis

--- components/Syllables/index.js
+++ components/Syllables/index.js
@@ -5,14 +5,31 @@ const renderSyllables = list => list.map((syl, idx, syls) => (
   <em key={syl}>{syl}{idx + 1 < syls.length ? '-' : ''}</em>
 ));
 
+// TODO: visually, it is implied that pronounce refers to the word.
+// Explore what aria roles would best suit this sitation. described-by?
+function speakPronunciation(e) {
+  e.preventDefault();
+  const message = new SpeechSynthesisUtterance();
+  const syllables = document.querySelector('.syllables');
+  const text = syllables.innerText
+    .substring(1, syllables.innerText.length - 1)
+    .replace(/-/g, '');
+
+  message.text = text;
+  speechSynthesis.speak(message);
+}
+
 const Syllables = ({ data }) => {
   if (data) {
     return (
       <div>
         <h3>syllables</h3>
         <p>
-        ({data.count} syllables) [{renderSyllables(data.list)}]
+        ({data.count} syllables) <span className="syllables">[{renderSyllables(data.list)}]</span>
         </p>
+        {'speechSynthesis' in window &&
+        <button onClick={speakPronunciation}>pronounce</button>
+        }
       </div>
     );
   }
